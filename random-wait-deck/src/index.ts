9d6f208f7ab5b329f198a11a834768887e5eeba4
Cameron Motevasselani <cameron@armory.io>
2023-06-13 12:13:38 -0500

    chore(ui): removing ui component

--- random-wait-deck/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { IDeckPlugin } from '@spinnaker/core';
-import { randomWaitStage } from './RandomWaitStage';
-import { initialize } from './initialize';
-
-export const plugin: IDeckPlugin = {
-  initialize,
-  stages: [randomWaitStage],
-};
